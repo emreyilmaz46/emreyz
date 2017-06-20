b31b34665ef89e7dc070f72422648acd3b8e6a3b
Emre YILMAZ <emreyilmaz46@gmail.com>
2017-06-20 00:39:48 +0000

    Kimlik sorunu çözüldü mü denemesi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    EmreYZ
                 </div>
 
                 <div class="links">
